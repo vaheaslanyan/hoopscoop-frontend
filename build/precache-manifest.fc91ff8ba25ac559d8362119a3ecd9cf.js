4f9bfc34dce9ed83363d77b71879b5f9e441d6fb
vaheaslanyan <vahe.sde@gmail.com>
2023-02-19 20:34:08 -0600

    Add callback to googleapi url

---
+++ build/precache-manifest.fc91ff8ba25ac559d8362119a3ecd9cf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cfa242f0327710a60f778b1bfd76a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58a3f48e760a6020200a",
+    "url": "/static/css/3.48bd79e1.chunk.css"
+  },
+  {
+    "revision": "69eae6f896430dad3755",
+    "url": "/static/css/4.dc5cd071.chunk.css"
+  },
+  {
+    "revision": "7062290da4c152f40a36",
+    "url": "/static/css/5.b6679c46.chunk.css"
+  },
+  {
+    "revision": "a18447390cb41fa112a1",
+    "url": "/static/css/main.61b3cd94.chunk.css"
+  },
+  {
+    "revision": "ca4c030ed89a4d4f5e3d",
+    "url": "/static/js/2.ceea05ba.chunk.js"
+  },
+  {
+    "revision": "58a3f48e760a6020200a",
+    "url": "/static/js/3.a95fdf72.chunk.js"
+  },
+  {
+    "revision": "69eae6f896430dad3755",
+    "url": "/static/js/4.ee30cdb9.chunk.js"
+  },
+  {
+    "revision": "7062290da4c152f40a36",
+    "url": "/static/js/5.1ad110b5.chunk.js"
+  },
+  {
+    "revision": "05beb4e11c79d1e7ef41",
+    "url": "/static/js/6.46c96d20.chunk.js"
+  },
+  {
+    "revision": "a18447390cb41fa112a1",
+    "url": "/static/js/main.946fdf65.chunk.js"
+  },
+  {
+    "revision": "083360865a67563c3630",
+    "url": "/static/js/runtime-main.b7c15598.js"
+  }
+]);
